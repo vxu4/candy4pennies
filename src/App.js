4ea6aa346c4983f51ca4cdd1df8b7fa66ece252c
vxu4 <45617317+vxu4@users.noreply.github.com>
2023-02-07 12:58:54 -0500

    initial component and app.js set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, createContext } from 'react';
 import './App.css';
+import "98.css";
+import Window from './components/window';
+
+export const AppContext = createContext();
 
 function App() {
+  // WINDOW SIZE MATCH BOOLEAN
+  // const [matches] = useState(
+  //   window.matchMedia("(min-width: 768px)").matches
+  // )
+  
+  // USE STATE EXAMPLE
+  // const [display, setDisplay] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //IF STATE IS USED - ADD THIS IN BELOW value={{ display, remember }}
+    <AppContext.Provider >
+      {/* POSITION: RELATIVE - SO THAT CHILDREN CAN BE ABSOLUTE AND LAYER? */}
+      <div className="App" style={{position: 'relative'}}>
+        <Window onClick={() => console.log("hihi")}/>
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/window.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+import "../App.css";
+import "98.css";
+
+const WindowComponent = ({ onClick }) => {
+  // const { display } = useContext(AppContext);
+  // if (display) {
+  <div
+    style={{ width: 300, marginRight: "3vw", marginBottom: "9vh" }}
+    className="window"
+  >
+    {/* TITLE BAR */}
+    <div className="title-bar">
+      <div className="title-bar-text">Transmissions: </div>
+    </div>
+
+    {/* BODY */}
+    <div className="window-body" style={{ textAlign: "right" }}>
+      How much capacity for love water must have, <br />
+      to carry generations in its memory. <br />
+      <br />
+      What love teachings can the water <br />
+      carry back to us? <br />
+      <br />
+      {onClick && (
+        <div
+          className="field-row"
+          style={{ justifyContent: "right", padding: "10px 0px 5px" }}
+        >
+          <button style={{ cursor: "pointer" }} onClick={onClick}>
+            repeat
+          </button>
+        </div>
+      )}
+    </div>
+  </div>;
+  // }
+};
+
+const Window = ({ onClick }) => {
+  return <WindowComponent onClick={onClick} />;
+};
+
+export default Window;
